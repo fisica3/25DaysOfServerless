137034b1094504c21165a952f078ef093a97cd9d
Ernesto Cárdenas <ernesto.cardenas@gmail.com>
2019-12-01 21:07:38 -0500

    Add project files.

---
+++ Prueba20/DreidelApi.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Prueba25
+{
+    //https://25daysofserverless.azurewebsites.net/calendar/1
+    public static class DreidelApi
+    {
+        [FunctionName("GetDreidel")]
+        public static IActionResult GetDreidel(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route="dreidel")]
+            HttpRequest req, ILogger log)
+        {            
+            var salutations = new string[] { "Nun", "Gimmel", "Hay", "Shin" };
+            try
+            {
+                log.LogInformation("Retrieving the Dreidel");
+                var rnd = new Random(System.DateTime.Now.Millisecond);
+                var indexSalutation = rnd.Next(0, 3);
+                var salutation = salutations[indexSalutation];
+                return new OkObjectResult(salutation);
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, ex.Message);
+                return new BadRequestResult();                
+            }
+            
+            
+        }
+
+    }
+}
